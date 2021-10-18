5dcc9ec904385006ebf1141c2a862ca602641346
Kim Corson <kimmc67b@gmail.com>
2021-10-18 14:29:49 -0400

    pushing department changes and pulling project

--- DataAccessObjects/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
+++ DataAccessObjects/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
@@ -1,12 +1,29 @@
 ﻿using ProjectOrganizer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace ProjectOrganizer.DAL
 {
+
+
     public class DepartmentSqlDAO : IDepartmentDAO
     {
         private readonly string connectionString;
+        private const string SqlSelect =
+        "SELECT department_id, name " +
+        "FROM department";
+
+        private const string SqlInsert =
+        "INSERT INTO department (department_id, name) " +
+        "VALUES (@department_id, @name)" +
+        "SELECT @@IDENTITY";
+
+
+        private const string SqlUpdate =
+        "UPDATE department" +
+        "SET (@department_id, @name)" +
+
 
         // Single Parameter Constructor
         public DepartmentSqlDAO(string dbConnectionString)
@@ -20,7 +37,35 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public ICollection<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            List<Department> departments = new List<Department>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlSelect, conn);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Department dept = new Department
+                        {
+                            Id = Convert.ToInt32(reader["department_id"]),
+                            Name = Convert.ToString(reader["name"])
+                        };
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Problem querying the database " + ex.Message);
+            }
+
+            return departments;
+
         }
 
         /// <summary>
@@ -28,9 +73,32 @@ namespace ProjectOrganizer.DAL
         /// </summary>
         /// <param name="newDepartment">The department object.</param>
         /// <returns>The id of the new department (if successful).</returns>
-        public int CreateDepartment(Department newDepartment)
+        public int CreateDepartment(Department newDepartment) //Insert
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlInsert, conn);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    Department dept = new Department();
+                    command.Parameters.AddWithValue("@department_id", dept.Id);
+                    command.Parameters.AddWithValue("@name", dept.Name);
+
+                    int id = Convert.ToInt32(command.ExecuteScalar());
+                    return id;
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Problem querying the database " + ex.Message);
+                return -1; // No department
+            }
         }
 
         /// <summary>
@@ -38,10 +106,31 @@ namespace ProjectOrganizer.DAL
         /// </summary>
         /// <param name="updatedDepartment">The department object.</param>
         /// <returns>True, if successful.</returns>
-        public bool UpdateDepartment(Department updatedDepartment)
+        public bool UpdateDepartment(Department updatedDepartment) // Update
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
 
+                    SqlCommand command = new SqlCommand(SqlUpdate, conn);
+
+                    Department dept = new Department();
+                    command.Parameters.AddWithValue("@department_id", dept.Id);
+                    command.Parameters.AddWithValue("@name", dept.Name);
+
+                    return true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Problem querying the database " + ex.Message);
+            }
+            return false;
+        }
     }
+
 }
+
+
