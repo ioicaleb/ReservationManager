2332a88c35371bb82f51ec123b4ee6a222bb8ced
Caleb Gaffney <ioicaleb12@gmail.com>
2021-10-19 13:03:49 -0400

    Started Day 2 assignment

---
+++ DataAccessObjects/ProjectOrganizerTests/ProjectTestsBase.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Transactions;
+
+namespace ProjectTests
+{
+    [TestClass]
+    public abstract class ProjectTestsBase
+    {
+        protected string ConnectionString { get; } = "Server=.\\SQLEXPRESS;Database=EmployeeDB;Trusted_Connection=True;";
+
+        private TransactionScope transaction;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            transaction = new TransactionScope();
+
+            string sql = File.ReadAllText("test-script.sql");
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Roll back the transaction
+            if (transaction != null)
+            {
+                transaction.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Gets the row count for a table.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        protected int GetRowCount(string table)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT COUNT(*) FROM {table}", conn);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return count;
+            }
+        }
+    }
+}
