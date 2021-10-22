b2515bdb88348f6954aa9802e21cf26d52c039aa
Kim Corson <kimmc67b@gmail.com>
2021-10-22 15:17:10 -0400

    pushing current version

--- Capstone/Capstone/DAL/ReservationDAO.cs
+++ Capstone/Capstone/DAL/ReservationDAO.cs
@@ -9,18 +9,13 @@ namespace Capstone.DAL
     public class ReservationDAO : IReservationDAO   
     {
         private const string SqlSelect =
-<<<<<<< HEAD
-            "SELECT r.id, r.space_id, r.number_of_attendees, r.start_date, r.end_date, r.reserved_for, s.name" +
-            "FROM reservation r INNER JOIN space s ON s.id = r.space_id " +
-            "WHERE r.space_id = @space_id"; // Requires filtering out any reservations that exist
-=======
+
             "SELECT s.id " +
             "FROM space s " +
             "WHERE s.venue_id = @venue_id " +
             "AND max_occupancy < @number_of_attendees " +
             "AND NOT EXISTS(SELECT * FROM reservation r " +
             "WHERE r.space_id = s.id AND start_date >= @start_date AND end_date <= @end_date)";
->>>>>>> 1350383ab49097f740b671bba6be261bcd4ecd5b
 
         private const string SqlCreateReservation =
             "INSERT INTO reservation(space_id, number_of_attendees, start_date, end_date, reserved_for) " +
@@ -34,12 +29,8 @@ namespace Capstone.DAL
             this.connectionString = connectionString;
         }
 
-<<<<<<< HEAD
-        // Returns a list of reservations that correspond to a specific space Id
-        public ICollection<Reservation> GetReservations(int spaceId)
-=======
+
         public List<int> GetReservations(int venueId, DateTime startDate, int stayLength, int numberOfAttendees)
->>>>>>> 1350383ab49097f740b671bba6be261bcd4ecd5b
         {
             List<int> spacesAvailable = new List<int>();
             try
@@ -70,7 +61,7 @@ namespace Capstone.DAL
             return spacesAvailable;
         }
 
-        public int ReserveSpace(int space_id, int numberOfAttendees, string startDate, int stayLength, string reservationName)
+        public int ReserveSpace(int space_id, int numberOfAttendees, DateTime startDate, int stayLength, string reservationName)
         {
             try
             {
